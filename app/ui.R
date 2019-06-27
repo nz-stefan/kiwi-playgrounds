b931741f6470d0eee97702b7f9bfcf0159bf1767
nz-stefan <sschliebs@gmail.com>
2019-06-27 23:08:16 +1200

    Code polished

--- app/ui.R
+++ app/ui.R
@@ -16,16 +16,3 @@ material_page(
   tags$style(type = "text/css", "#playground_map {height: calc(100vh - 64px) !important;}"),
   leafletOutput("playground_map")
 )
-
-# fluidPage(
-#   # add CSS customizations
-#   tags$head(
-#     tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css")
-#   ),
-#   
-#   div(
-#     class = "card",
-#     tags$style(type = "text/css", "#playground_map {height: calc(100vh - 20px) !important;}"),
-#     leafletOutput("playground_map")
-#   )
-# )
