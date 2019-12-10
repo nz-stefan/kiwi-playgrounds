2768a31eddfbe5046c1dd8ef9b7e970ec56d5c14
nz-stefan <sschliebs@gmail.com>
2019-12-10 22:35:32 +1300

    Basic functionality polished

--- app/server.R
+++ app/server.R
@@ -8,7 +8,14 @@
 
 server <- function(input, output, session) {
   
-  d_parks_geocoded <- reactive(readr::read_csv("data/parks_geocoded.csv"))
+  d_parks_geocoded <- reactive({
+    # load data from file and create the HTML for the popup label
+    readr::read_csv("data/parks_geocoded.csv") %>% 
+      mutate(
+        button = sprintf("<button onclick='Shiny.onInputChange(\"popup_click\",  %d)' id='popup_click' type='button' class='waves-effect waves-light btn shiny-material-button z-depth-2 light-blue lighten-1'><i class='material-icons left'>info_outline</i>Info</button>", id),
+        html_name = sprintf("<p style='font-weight: bold; font-size: 1.5em'>%s</p><p>%s</p>%s", name, address, button)
+      )
+  })
   
   output$playground_map <- renderLeaflet({
     req(d_parks_geocoded())
@@ -35,12 +42,42 @@ server <- function(input, output, session) {
         )
       ) %>% 
       htmlwidgets::onRender("function(el, x) {L.control.zoom({ position: 'bottomright' }).addTo(this)}") %>%
-      addMarkers(lng = ~long, lat = ~lat, popup = ~name, clusterOptions = markerClusterOptions()) %>% 
+      addMarkers(lng = ~long, lat = ~lat, popup = ~html_name, layerId = ~id, clusterOptions = markerClusterOptions()) %>% 
       fitBounds(
         min(d_parks_geocoded()$long), 
         min(d_parks_geocoded()$lat), 
         max(d_parks_geocoded()$long), 
         max(d_parks_geocoded()$lat)
       )
+  })
+  
+
+  # Capture marker click events ---------------------------------------------
+  
+  observeEvent(input$playground_map_marker_click, {
+    req(d_parks_geocoded())
+    
+    # Map events are captured in input variable input$MAPID_OBJCATEGORY_EVENTNAME.
+    # The object category for markers is "marker", the event name is "click".
+    event <- input$playground_map_marker_click
+    
+    print(d_parks_geocoded() %>% filter(id == event$id))
+    
+  })
+  
+  observeEvent(input$popup_click, {
+    # identify playground record linked to clicked popup
+    clicked_playground <- 
+      d_parks_geocoded() %>% 
+      filter(id == input$popup_click)
+    
+    print(input$popup_click)
+    
+    # showModal(
+    #   modalDialog(
+    #     title = clicked_playground$name,
+    #     tags$p(clicked_playground$equipment)
+    #   )
+    # )
   })
 }
--- app/ui.R
+++ app/ui.R
@@ -9,10 +9,20 @@
 material_page(
   title = "Kiwi Playgrounds",
   nav_bar_color =  "grey darken-4",
+
+  # add CSS customizations
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css")
+  ),
+  
+  # add side navigation
   material_side_nav(
     fixed = FALSE,
     tags$h3("Some more content here")
   ),
+  
+  # add map
+  # we let the browser compute the window height to dynamically fill the entire screen with the map
   tags$style(type = "text/css", "#playground_map {height: calc(100vh - 64px) !important;}"),
   leafletOutput("playground_map")
 )
